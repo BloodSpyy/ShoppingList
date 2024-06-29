24aceb4125a4a1849d1eef8adacfd5faca13d822
Gleb Reshetov <hahaclassik@gmail.com>
2024-06-29 16:32:01 +0400

    1. Create packages with name: data, domain, presentation. 2. Move MainActivity.kt to presentation package

--- app/src/main/java/com/bloodspy/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/bloodspy/shoppinglist/presentation/MainActivity.kt
@@ -1,10 +1,8 @@
-package com.bloodspy.shoppinglist
+package com.bloodspy.shoppinglist.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.bloodspy.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
