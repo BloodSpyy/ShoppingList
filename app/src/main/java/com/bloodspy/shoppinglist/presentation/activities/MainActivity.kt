3a018d24ad633e175663b7aadd940be0710426be
Gleb Reshetov <hahaclassik@gmail.com>
2024-08-05 14:29:19 +0400

    Add data binding to MainActivity

--- app/src/main/java/com/bloodspy/shoppinglist/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/bloodspy/shoppinglist/presentation/activities/MainActivity.kt
@@ -9,35 +9,32 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.bloodspy.shoppinglist.R
+import com.bloodspy.shoppinglist.databinding.ActivityMainBinding
 import com.bloodspy.shoppinglist.presentation.viewmodels.MainViewModel
 import com.bloodspy.shoppinglist.presentation.fragments.ShopItemFragment
 import com.bloodspy.shoppinglist.presentation.recyclerViewUtils.adapters.ShoppingListAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(), ShopItemFragment.OnEndWorkListener {
+    private lateinit var binding: ActivityMainBinding
+
     private lateinit var viewModel: MainViewModel
 
-    private lateinit var recyclerViewShoppingList: RecyclerView
     private lateinit var shoppingListAdapter: ShoppingListAdapter
-
-    private lateinit var buttonAddShopItem: FloatingActionButton
 
     private lateinit var orientation: String
 
-    private var shopItemContainer: FragmentContainerView? = null
-
     companion object {
-        private const val LOG_TAG = "MainActivity"
-
         private const val PORTRAIT_ORIENTATION = "portrait"
         private const val LANDSCAPE_ORIENTATION = "landscape"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        initViews()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         setupViewModel()
         observeViewModel()
         parseOrientation()
@@ -46,20 +43,17 @@ class MainActivity : AppCompatActivity(), ShopItemFragment.OnEndWorkListener {
     }
 
     private fun setupOnClickListeners() {
-        if(orientation == PORTRAIT_ORIENTATION) {
-            buttonAddShopItem.setOnClickListener {
-                startActivity(ShopItemActivity.newIntentAddItem(this))
-            }
-        }
-        else {
-            buttonAddShopItem.setOnClickListener {
-                    launchFragment(ShopItemFragment.newInstanceAddItem())
+        binding.buttonAddShopItem.setOnClickListener {
+            if(orientation == PORTRAIT_ORIENTATION) {
+                startActivity(ShopItemActivity.newIntentAddItem(this@MainActivity))
+            } else {
+                launchFragment(ShopItemFragment.newInstanceAddItem())
             }
         }
     }
 
     private fun parseOrientation() {
-        orientation = if(shopItemContainer == null) {
+        orientation = if(binding.shopItemContainer == null) {
             PORTRAIT_ORIENTATION
         } else {
             LANDSCAPE_ORIENTATION
@@ -75,10 +69,8 @@ class MainActivity : AppCompatActivity(), ShopItemFragment.OnEndWorkListener {
     }
 
     private fun setupRecyclerView() {
-        recyclerViewShoppingList = findViewById(R.id.recyclerViewShoppingList)
-        shoppingListAdapter = ShoppingListAdapter()
-
-        with(recyclerViewShoppingList) {
+        with(binding.recyclerViewShoppingList) {
+            shoppingListAdapter = ShoppingListAdapter()
             adapter = shoppingListAdapter
             recycledViewPool.setMaxRecycledViews(
                 ShoppingListAdapter.SHOP_ITEM_ENABLED_VIEW_TYPE,
@@ -111,11 +103,6 @@ class MainActivity : AppCompatActivity(), ShopItemFragment.OnEndWorkListener {
 
     private fun setupViewModel() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-    }
-
-    private fun initViews() {
-        buttonAddShopItem = findViewById(R.id.buttonAddShopItem)
-        shopItemContainer = findViewById(R.id.shopItemContainer)
     }
 
     private fun setupSwipeListener(recyclerViewShoppingList: RecyclerView) {
