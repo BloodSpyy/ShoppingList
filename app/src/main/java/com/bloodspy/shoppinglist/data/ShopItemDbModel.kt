c4bacffc1968627dda18bc0e0b3788b1f73baa06
Gleb Reshetov <hahaclassik@gmail.com>
2024-08-10 15:48:56 +0400

    Add primary key to shopItemDbModel

--- app/src/main/java/com/bloodspy/shoppinglist/data/ShopItemDbModel.kt
+++ app/src/main/java/com/bloodspy/shoppinglist/data/ShopItemDbModel.kt
@@ -1,9 +1,11 @@
 package com.bloodspy.shoppinglist.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "shop_items")
 data class ShopItemDbModel(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val name: String,
     val count: Int,
